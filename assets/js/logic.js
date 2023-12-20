d20f67f268d20b504bcf5a15fb64bb8e83e4ed17
Jonno Clifford <jma.clifford@gmail.com>
2023-12-20 15:01:12 +0000

    Timer added - counting down from 60secs, alert, pop up added if timer runs out

---
+++ assets/js/logic.js
@@ -0,0 +1,26 @@
+document.getElementById('start').addEventListener('click', function () {
+  startTimer(60);
+});
+
+let timerInterval;
+let timerValue;
+
+function startTimer(initialSeconds) {
+  timerValue = initialSeconds * 1000;
+  updateTimerDisplay();
+
+  timerInterval = setInterval(function () {
+    timerValue -= 10;
+    updateTimerDisplay();
+
+    if (timerValue <= 0) {
+      clearInterval(timerInterval);
+      alert("You've run out of time!");
+    }
+  }, 10);
+}
+
+function updateTimerDisplay() {
+  const seconds = (timerValue / 1000).toFixed(2);
+  document.getElementById('time').textContent = seconds;
+}
